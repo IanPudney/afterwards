9fa269599eaab1dce5d447a87d1320dfe6615660
pwstegman <pwstegman@gmail.com>
2014-09-13 13:43:31 -0500

    Added collider

---
+++ Assets/Laser.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 1f;
+	public Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+		dir = target.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(dir * speed * Time.deltaTime, Space.World);
+	}
+}
