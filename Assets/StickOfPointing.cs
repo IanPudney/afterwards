3b075380f8b5bf285a53fc54915eeea3e4401812
IanPudney <ipudney@umich.edu>
2014-09-14 03:51:51 -0400

    The stick of pointing

---
+++ Assets/StickOfPointing.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickOfPointing : MonoBehaviour {
+	public Blade blade;
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x,
+		                                             blade.transform.eulerAngles.y - offset.y,
+		                                             transform.rotation.eulerAngles.z));
+	}
+}
